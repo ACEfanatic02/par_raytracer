ea78d06d65022af5989a43dd6968cc2d732356d2
Bryan Taylor <acefanatic02@gmail.com>
2017-03-11 14:14:49 -0500

    fix g++ compile issues

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 mkdir -p build
-g++ -g -O2 --std=c++11 -fno-strict-aliasing -m64 -Wall -o ./build/raytracer ./raytracer.cpp
+g++ -g -O2 --std=c++11 -fno-strict-aliasing -m64 -Wall -o ./build/raytracer ./main.cpp
